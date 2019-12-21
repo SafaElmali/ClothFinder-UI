af5fbfa22264adb2cbdf0b53477b4eeb0d2d70b8
SafaElmali <tsafaelmali@gmail.com>
2019-12-21 04:11:32 +0300

    updated startpoint

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,9 @@
+let startpoint = "https://finder-262622.appspot.com/";
+
 export let loginRecepPoint = "http://192.168.0.10:8080/login";
 export let registerRecepPoint = "http://192.168.0.10:8080/register";
 export let garmentRecepPoint = "http://192.168.0.10:8080/garments/";
 
-export let loginLocalPoint = "http://localhost:8080/login";
-export let registerLocalPoint = "http://localhost:8080/register"
-export let garmentTypeLocalPoint = "http://localhost:8080/garments/";
-
+export let loginLocalPoint = `${startpoint}login`;
+export let registerLocalPoint = `${startpoint}register`;
+export let garmentTypeLocalPoint = `${startpoint}garments/`;
