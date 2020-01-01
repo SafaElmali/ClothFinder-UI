22aa7fcfcbc5fca05b60d56e1aa54601f644900c
SafaElmali <tsafaelmali@gmail.com>
2020-01-01 15:00:01 +0300

    styles to another file

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    storyView: {
+        flex: 0.9,
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
+    },
+});
+
+export default styles;
