7060aed010b0dece0893d90ce4eb4be633afd36c
SafaElmali <tsafaelmali@gmail.com>
2020-01-15 20:59:45 +0300

    added swiper

--- src/screens/Home/components/Ratings/index.js
+++ src/screens/Home/components/Ratings/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { WorstEmoji, BestEmoji, BadEmoji, NeutralEmoji, GoodEmoji } from '../../../../components/SvgFiles/index';
+import Swiper from 'react-native-swiper'
 import { Icon } from 'react-native-elements';
 import styles from './styles';
 
@@ -15,41 +16,68 @@ const Rating = props => {
     }, [outfit])
 
     return (
-        <View style={styles.ratingCard}>
-            <View style={styles.questionContainer}>
-                <Text style={styles.questionText}>How do you feel with selected SelectedItem ({1}/{2})?</Text>
-            </View>
-            <View style={styles.selectionContainer}>
-                <View style={styles.arrowContainer}>
-                    <Icon name='chevron-left' size={30} onPress={() => console.log(arr)} />
-                </View>
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.buttonCenterView}>
-                        <WorstEmoji />
-                        <Text style={styles.buttonText}>WORST</Text>
+        <>
+            <Swiper showsButtons={true} showsPagination={false}>
+                <View style={styles.ratingCard}>
+                    <View style={styles.questionContainer}>
+                        <Text style={styles.questionText}>How do you feel with selected SelectedItem ({1}/{2})?</Text>
                     </View>
-                    <View style={styles.buttonCenterView}>
-                        <BadEmoji />
-                        <Text style={styles.buttonText}>BAD</Text>
-                    </View>
-                    <View style={styles.buttonCenterView}>
-                        <NeutralEmoji />
-                        <Text style={styles.buttonText}>NEUTRAL</Text>
-                    </View>
-                    <View style={styles.buttonCenterView}>
-                        <GoodEmoji />
-                        <Text style={styles.buttonText}>GOOD</Text>
-                    </View>
-                    <View style={styles.buttonCenterView}>
-                        <BestEmoji />
-                        <Text style={styles.buttonText}>BEST</Text>
-                    </View>
-                    <View style={styles.arrowContainer}>
-                        <Icon name='chevron-right' size={30} onPress={() => console.log(arr)} />
+                    <View style={styles.selectionContainer}>
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.buttonCenterView}>
+                                <WorstEmoji />
+                                <Text style={styles.buttonText}>WORST</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <BadEmoji />
+                                <Text style={styles.buttonText}>BAD</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <NeutralEmoji />
+                                <Text style={styles.buttonText}>NEUTRAL</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <GoodEmoji />
+                                <Text style={styles.buttonText}>GOOD</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <BestEmoji />
+                                <Text style={styles.buttonText}>BEST</Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
-            </View>
-        </View>
+                <View style={styles.ratingCard}>
+                    <View style={styles.questionContainer}>
+                        <Text style={styles.questionText}>How do you feel with selected SelectedItem ({1}/{2})?</Text>
+                    </View>
+                    <View style={styles.selectionContainer}>
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.buttonCenterView}>
+                                <WorstEmoji />
+                                <Text style={styles.buttonText}>WORST</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <BadEmoji />
+                                <Text style={styles.buttonText}>BAD</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <NeutralEmoji />
+                                <Text style={styles.buttonText}>NEUTRAL</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <GoodEmoji />
+                                <Text style={styles.buttonText}>GOOD</Text>
+                            </View>
+                            <View style={styles.buttonCenterView}>
+                                <BestEmoji />
+                                <Text style={styles.buttonText}>BEST</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </Swiper>
+        </>
     )
 }
 
