0375878dc4b64ce8e04a96ac174672318cb20541
SafaElmali <tsafaelmali@gmail.com>
2020-01-22 23:32:25 +0300

    added rating area

--- src/screens/Home/components/Ratings/index.js
+++ src/screens/Home/components/Ratings/index.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import { Text } from 'react-native-elements';
 import { WorstEmoji, BestEmoji, BadEmoji, NeutralEmoji, GoodEmoji } from '../../../../components/SvgFiles/index';
 import DisabledCard from '../DisabledCard/index';
+import Carousel from 'react-native-snap-carousel';
 import styles from './styles';
 
 const Rating = props => {
     const { outfit } = props;
     const [outfitList, setOutfitList] = useState([]);
-    console.log(outfitList);
+    const selectedList = [];
 
+    // Get all selected outfits into an array
+    outfitList.forEach(outfitArray => {
+        if (outfitArray.length > 0) {
+            outfitArray.forEach(garmentItem => {
+                selectedList.push(garmentItem)
+            });
+        }
+    })
+
+    // Update outfitList if garment item added or removed
     useEffect(() => {
         setOutfitList(Object.values(outfit));
     }, [outfit])
 
     const assignRate = (item, rateType, garmentType) => {
         const { handleSelectedRate } = props;
-        const [topwearList, bottomwearList, footwearList, accessoriesList] = arr;
+        const [topwearList, bottomwearList, footwearList, accessoriesList] = outfitList;
         let selectedGarmentItem = -1;
 
         if (garmentType === 'TOPWEAR') {
@@ -40,9 +51,53 @@ const Rating = props => {
         }
     }
 
+    const _renderItem = ({ item, index }) => {
+        return (
+            <View key={index} style={styles.ratingCard}>
+                <View style={styles.questionContainer}>
+                    <Text style={styles.questionText}>How do you feel with selected {item.garment.name}?</Text>
+                </View>
+                <View style={styles.selectionContainer}>
+                    <View style={styles.buttonsContainer}>
+                        <View style={styles.buttonCenterView}>
+                            <WorstEmoji onPress={() => assignRate(item, 'WORST', item.garment.garmentType)} />
+                            <Text style={styles.buttonText}>WORST</Text>
+                        </View>
+                        <View style={styles.buttonCenterView}>
+                            <BadEmoji onPress={() => assignRate(item, 'BAD', item.garment.garmentType)} />
+                            <Text style={styles.buttonText}>BAD</Text>
+                        </View>
+                        <View style={styles.buttonCenterView}>
+                            <NeutralEmoji onPress={() => assignRate(item, 'NEUTRAL', item.garment.garmentType)} />
+                            <Text style={styles.buttonText}>NEUTRAL</Text>
+                        </View>
+                        <View style={styles.buttonCenterView}>
+                            <GoodEmoji onPress={() => assignRate(item, 'GOOD', item.garment.garmentType)} />
+                            <Text style={styles.buttonText}>GOOD</Text>
+                        </View>
+                        <View style={styles.buttonCenterView}>
+                            <BestEmoji onPress={() => assignRate(item, 'BEST', item.garment.garmentType)} />
+                            <Text style={styles.buttonText}>BEST</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
+
     return (
         <>
-            <DisabledCard />
+            {
+                selectedList.length > 0 ?
+                    <Carousel
+                        data={selectedList}
+                        renderItem={_renderItem}
+                        sliderWidth={Dimensions.get('screen').width}
+                        itemWidth={300}
+                    />
+                    : <DisabledCard />
+            }
         </>
     )
 }
